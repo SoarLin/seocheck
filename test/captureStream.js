3672e0a3456f768becb43d267c690d46d4524873
Soar <soar.lin@gmail.com>
2018-01-31 10:25:25 +0800

    still can not capture process.stdout

---
+++ test/captureStream.js
@@ -0,0 +1,24 @@
+function captureStream(stream) {
+  var oldWrite = stream.write;
+  var buf = '';
+
+  stream.write = function(chunk, encoding, callback){
+    buf += chunk.toString(); // chunk is a String or Buffer
+    oldWrite.apply(stream, arguments);
+
+    if (typeof callback === 'function') {
+      callback();
+    }
+  };
+
+  return {
+    unhook: function unhook() {
+      stream.write = oldWrite;
+    },
+    captured: function() {
+      return buf;
+    }
+  };
+}
+
+module.exports = captureStream;
--- test/test.js
+++ test/test.js
@@ -2,22 +2,21 @@
 const chai = require('chai');
 const assert = chai.assert;
 const SEOChecker = require('../lib/index');
+const captureStream = require('./captureStream');
 
-describe('input: HTML file, output: console', function() {
+describe('input: HTML file, check property', function() {
   var checker;
   beforeEach(function() {
     checker = new SEOChecker({
       input: __dirname + '/index.html',
       output: 'console',
       maxStrongTags: 17,
-      rules: [
-        SEOChecker.imgShouldContainAltAttr,
-        SEOChecker.linkShouldContainRelAttr,
-        SEOChecker.bodySholdNotContainMoreThanOneH1,
-        SEOChecker.headShouldContainMetaAndTitle,
-        SEOChecker.bodySholdNotContainTooMoreStrong
-      ]
+      rules: []
     });
+  });
+
+  afterEach(function() {
+    checker = null;
   });
 
   describe('SEOChecker property', function() {
@@ -40,5 +39,43 @@ describe('input: HTML file, output: console', function() {
     it('maxStrongTags should be 17', function() {
       assert.equal(checker.maxStrongTags, 17);
     });
+
+    it('check rules length should be 1', function() {
+      checker.addRules(SEOChecker.imgShouldContainAltAttr);
+      assert.equal(checker.rules.length, 1);
+    });
+  });
+});
+
+describe('input: HTML file, output: console', function() {
+  var checker, hook;
+  beforeEach(function() {
+    checker = new SEOChecker({
+      input: __dirname + '/index.html',
+      output: 'console',
+      maxStrongTags: 17,
+      rules: [SEOChecker.imgShouldContainAltAttr]
+    });
+
+    hook = new captureStream(process.stdout);
+  });
+
+  afterEach(function() {
+    hook.unhook();
+    checker = null;
+  });
+
+  it('image should contain alt attribute', function() {
+    // return new Promise((resolve) => {
+    //   resolve(checker.check());
+    // }).then(()=>{
+    //   assert.equal(hook.captured(), 'There are 4 <img> without alt attritube');
+    // });
+
+    // assert.equal(hook.captured(), 'There are 4 <img> without alt attritube');
+    // assert.match(hook.captured(), '/without alt attritube/');
+
+    console.log('hellow');
+    assert.equal(hook.captured(), 'hellow\n');
   });
 });
